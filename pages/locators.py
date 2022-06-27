a12620b231059cfc727e693a71b0b84c80b7691b
Lolu4 <zelyaninvictor@gmail.com>
2022-06-27 10:06:12 +0300

    fix

--- pages/locators.py
+++ pages/locators.py
@@ -15,7 +15,7 @@ class ProductPageLocators():
     NAME_PRODUCT = (By.TAG_NAME, "h1")
     PRICE_PRODUCT = (By.CLASS_NAME, "price_color")
     NAME_PRODUCT_IN_BASKET = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
-    PRICE_PRODUCT_IN_BASKET = (By.XPATH, '//*[@id="messages"]/div[5]/div/p[1]/strong')
-
+    #PRICE_PRODUCT_IN_BASKET = (By.XPATH, '/ html / body / div[2] / div / div[1] / div[3] / div / p[1] / strong')
+    PRICE_PRODUCT_IN_BASKET = (By.CSS_SELECTOR, '.col-sm-6>.price_color')
 
 
--- pages/product_page.py
+++ pages/product_page.py
@@ -22,3 +22,13 @@ class ProductPage(BasePage):
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def product_in_basket(self):
+        product_name = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
+        product_basket_name = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_IN_BASKET).text
+        assert product_name == product_basket_name, "The product name in the cart is not correct"
+
+    def correct_price_in_basket(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
+        product_price_in_busket = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_IN_BASKET).text
+        assert product_price in product_price_in_busket, "The price of the item in the cart is not correct"
